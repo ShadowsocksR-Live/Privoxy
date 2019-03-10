f6495fa8a3f44f87b64354b6b7338492da8d7de3
ssrlive <ssrlivebox@gmail.com>
2019-03-10 14:58:16 +0800

    __cplusplus issue.

--- privoxyexports.h
+++ privoxyexports.h
@@ -1,8 +1,17 @@
 #ifndef __PRIVOXY_EXPORTS_H__
 #define __PRIVOXY_EXPORTS_H__ 1
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 typedef void (*privoxy_cb)(int fd, void *data);
 extern int privoxy_main_entry(const char *conf_path, privoxy_cb cb, void *data);
 extern void privoxy_shutdown(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __PRIVOXY_EXPORTS_H__ */
