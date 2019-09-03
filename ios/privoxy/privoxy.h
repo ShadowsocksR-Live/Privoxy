37c80ca40e1c3107a02d5afc28a227394c9b9c5c
ssrlive <ssrlivebox@gmail.com>
2019-09-04 03:33:30 +0800

    iOS / macOS settings.

---
+++ ios/privoxy/privoxy.h
@@ -0,0 +1,19 @@
+//
+//  privoxy.h
+//  privoxy
+//
+//  Created by ssrlive on 2019/9/3.
+//  Copyright © 2019 ssrlive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for privoxy.
+FOUNDATION_EXPORT double privoxyVersionNumber;
+
+//! Project version string for privoxy.
+FOUNDATION_EXPORT const unsigned char privoxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <privoxy/PublicHeader.h>
+
+#import <privoxy/privoxyexports.h>
